ec20a46b54f7105bfd6bf540c051b39e13f175c7
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2025-07-30 14:05:19 +0200

    php warning fix

--- app/models/Driver.php
+++ app/models/Driver.php
@@ -364,7 +364,7 @@ class Driver
                     $isValid = false;
                     break;
                 }
-                if ($server_info['maxParticipants'] != '' && $server_info['maxParticipants'] > 0) {
+                if (isset($values['maxParticipants']) && $server_info['maxParticipants'] != '' && $server_info['maxParticipants'] > 0) {
                     if ($values['minParticipants'] > $server_info['maxParticipants']) {
                         $isValid = false;
                         break;
